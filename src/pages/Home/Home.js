a89408cd2571b288c00be8410ae602c6cbe250b4
kadek surya mahardika <kadeksuryam@gmail.com>
2021-07-06 14:24:39 +0700

    edit banner desc

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,11 +5,10 @@ import './Home.css'
 const Home = () => {
     return (
         <Jumbotron className="home-container">
-            <div className="home-banner-title">Welcome to Our Site</div>
+            <div className="home-banner-title">Welcome to Our Hospital Information System</div>
             <div className="home-banner-content">
-                We provide a service that is you can book a doctor from this site by making 
-                an appointment with our doctors. You need an account to book a doctor,
-                if you dont have yet, you can create <Link to="/signup">here</Link>
+                We provide a service that is you can apply an appointment with our doctors.
+                You need an account to apply an appointment, if you dont have yet, you can create <Link to="/signup">here</Link>
             </div>
             <div className="home-banner-appointment">
                 <Button as={Link} to="/doctors" variant="primary">List of Doctors</Button>
